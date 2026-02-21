8252e4102e404edfd43e84c6412f6ae52fe1f3e0
Mahmoud Salah <mahmoud.kabieel@gmail.com>
2026-02-21 23:09:45 +0200

    Array.prototype.filter

---
+++ arrays/filter.js
@@ -0,0 +1,13 @@
+// function filtter(callback, thisArg) {
+//   let newArr = [];
+
+//   for (let i = 0; i < array.length; i++) {
+//      if (callback(ar)) {
+
+//      }
+//   }
+// }
+
+// let nums = [1, 2, 3, 4, 5];
+
+// nums.filter((num) => num > 4);
